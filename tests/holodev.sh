acca97c753efe9063837ec41fdea7f5790ff4e1a
Joenio Costa <joenio@colivre.coop.br>
2016-02-24 20:33:13 -0300

    added very simple test for setup() method

--- tests/holodev.sh
+++ tests/holodev.sh
@@ -5,7 +5,17 @@ test_code_returned_by_holodev_without_no_arguments() {
   assertEquals "should return '2' when has no arguments in command line!" 2 $?
 }
 
-test_code_returned_by_holodev_with_invalid_arguments() {
+test_code_returned_by_invalid_arguments() {
   ./holodev invalid-crazy-argument > /dev/null 2>&1
   assertEquals "should return '3' for invalid arguments!" 3 $?
+}
+
+test_code_returned_by_valid_arguments() {
+  ./holodev info > /dev/null 2>&1
+  assertEquals "should return '0' for valid arguments!" 0 $?
+}
+
+test_setup() {
+  ./holodev setup > /dev/null 2>&1
+  assertEquals "should return '0' when setup is ok!" 0 $?
 }
