b3c10fb3bc5a40ac6e64065ce961ade4b66005b0
Joenio Costa <joenio@colivre.coop.br>
2016-02-24 21:34:34 -0300

    fix bug and add test for 'holodev run' command

--- tests/holodev.sh
+++ tests/holodev.sh
@@ -19,3 +19,23 @@ test_setup() {
   ./holodev setup > /dev/null 2>&1
   assertEquals "should return '0' when setup is ok!" 0 $?
 }
+
+test_create_and_destroy_container_with_default_options() {
+  ./holodev create > /dev/null 2>&1
+  assertEquals "should create LXC container!" 0 $?
+  ./holodev destroy > /dev/null 2>&1
+  assertEquals "should destroy LXC container!" 0 $?
+}
+
+test_create_container_and_mount_the_current_directory_into_container() {
+  WORKING_DIR=`pwd`
+  TEMP_DIR=`mktemp --directory`
+  cd $TEMP_DIR
+  touch file.txt
+  $WORKING_DIR/holodev create > /dev/null 2>&1
+  COUNT=`$WORKING_DIR/holodev run ls | grep --count file.txt`
+  assertEquals "'file.txt' exists in the LXC container!" 1 $COUNT
+  $WORKING_DIR/holodev destroy > /dev/null 2>&1
+  cd $WORKING_DIR
+  rm -rf $TEMP_DIR
+}
