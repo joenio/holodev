a237e441e7de5fc76e404391e14358da4bce6a9b
Joenio Costa <joenio@colivre.coop.br>
2016-02-25 15:29:25 -0300

    avoid Debian dialog when installing lxc package

--- development-setup.sh
+++ development-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export DEBIAN_FRONTEND=noninteractive
+echo "lxc lxc/directory string /var/lib/lxc" | sudo debconf-set-selections
 sudo apt-get update
-which mk-build-deps || sudo apt-get install -y devscripts
+which mk-build-deps || sudo apt-get install -y devscripts equivs
 dpkg-checkbuilddeps || yes | sudo mk-build-deps --install --remove
