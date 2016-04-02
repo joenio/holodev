8b1d7e81a31c522accc777adb2882a2dfb6303ee
Joenio Costa <joenio@colivre.coop.br>
2016-04-02 13:02:07 -0300

    setup script for development under Archlinux

---
+++ development-setup/Arch.sh
@@ -0,0 +1,12 @@
+yaourt -S --force --noconfirm \
+        bash-completion \
+        bridge-utils \
+        cgmanager \
+        debootstrap \
+        dnsmasq \
+        ebtables \
+        git \
+        libvirt \
+        lxc \
+        shunit2 \
+        sudo \
