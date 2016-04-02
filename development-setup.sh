a10c1e59fdf8124849a1daa9672aa8edc2d52d22
Joenio Costa <joenio@colivre.coop.br>
2016-04-02 13:16:35 -0300

    warning when script for some Linux dist not found

--- development-setup.sh
+++ development-setup.sh
@@ -7,12 +7,16 @@ elif [ -f /etc/debian_version ]; then
   OS=Debian
 elif [ -f /etc/arch-release ]; then
   OS=Arch
+elif [ -f /etc/SuSE-release ]; then
+  OS=Suse
 else
   OS=$(uname -s)
 fi
 
 if [ -x ./development-setup/$OS.sh ]; then
   ./development-setup/$OS.sh
+  ./holodev setup
+else
+  echo "W: ./development-setup/$OS.sh not found, aborting setup for holodev!"
+  echo "W: You need to install the development dependencies by hand."
 fi
-
-./holodev setup
