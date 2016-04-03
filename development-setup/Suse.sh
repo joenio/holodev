076e768995716dd865347426a9176f5694ac7c42
Joenio Costa <joenio@colivre.coop.br>
2016-04-03 13:25:32 -0300

    development script for openSUSE
    some tips about the Apparmor, libvirt and lxc:
    * http://help.ubuntu.com/lts/serverguide/lxc.html#lxc-apparmor

---
+++ development-setup/Suse.sh
@@ -0,0 +1,24 @@
+zypper --non-interactive in \
+        bash-completion \
+        bridge-utils \
+        debootstrap \
+        dnsmasq \
+        ebtables \
+        git \
+        libvirt \
+        lxc \
+        shunit2 \
+        sudo \
+
+echo 'PATH=$PATH:/usr/share/shunit2/src' >> /etc/profile.local
+
+which aa-status > /dev/null; if [ $? -ne 0 ]; then
+  echo
+  echo "aa-status not found!" 1>&2
+  echo "Disabling LXC Apparmor profile"
+  mkdir /etc/apparmor.d/disabled/
+  ln -s /etc/apparmor.d/usr.bin.lxc-start /etc/apparmor.d/disabled/
+fi
+
+systemctl start libvirtd
+systemctl enable libvirtd
