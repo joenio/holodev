6529ada91cc1aaad0e51a20ce4072a7b7a6688d9
Joenio Costa <joenio@colivre.coop.br>
2016-04-22 01:00:07 -0300

    adding --dummy option
    also:
    * bash-completion a litle bit more smart
    * not repeat sudo everywhere

--- tests/minimal_test.sh
+++ tests/minimal_test.sh
@@ -34,6 +34,43 @@ test_container_name_under_directory() {
   CONTAINER_NAME=$(../../holodev info 2> /dev/null | sed 's/.\+info about //' | sed "s/'//g")
   COUNT=$(echo $CONTAINER_NAME | grep -c 'directory')
   assertEquals "container name '$CONTAINER_NAME' isn't 'directory'" 1 $COUNT
+  cd ../../
+}
+
+test_create_command_with_dummy_option() {
+  HOLODEV_OUTPUT=$(./holodev create --dummy 2>&1)
+  COUNT=$(echo $HOLODEV_OUTPUT | grep -c 'sudo lxc-create -n ')
+  assertEquals "'create' command didn't run 'lxc-create'" 1 $COUNT
+}
+
+test_attach_command_with_dummy_option() {
+  HOLODEV_OUTPUT=$(./holodev attach --dummy 2>&1)
+  COUNT=$(echo $HOLODEV_OUTPUT | grep -c 'sudo lxc-attach -n ')
+  assertEquals "'attach' command didn't run 'lxc-attach'" 1 $COUNT
+}
+
+test_destroy_command_with_dummy_option() {
+  HOLODEV_OUTPUT=$(./holodev destroy --dummy 2>&1)
+  COUNT=$(echo $HOLODEV_OUTPUT | grep -c 'sudo lxc-destroy -n ')
+  assertEquals "'destroy' command didn't run 'lxc-destroy'" 1 $COUNT
+}
+
+test_start_command_with_dummy_option() {
+  HOLODEV_OUTPUT=$(./holodev start --dummy 2>&1)
+  COUNT=$(echo $HOLODEV_OUTPUT | grep -c 'sudo lxc-start -n ')
+  assertEquals "'start' command didn't run 'lxc-start'" 1 $COUNT
+}
+
+test_stop_command_with_dummy_option() {
+  HOLODEV_OUTPUT=$(./holodev stop --dummy 2>&1)
+  COUNT=$(echo $HOLODEV_OUTPUT | grep -c 'sudo lxc-stop -n ')
+  assertEquals "'stop' command didn't run 'lxc-stop'" 1 $COUNT
+}
+
+test_list_command_with_dummy_option() {
+  HOLODEV_OUTPUT=$(./holodev list --dummy 2>&1)
+  COUNT=$(echo $HOLODEV_OUTPUT | grep -c 'sudo lxc-ls -1')
+  assertEquals "'list' command didn't run 'lxc-ls'" 1 $COUNT
 }
 
 . shunit2
