c214044ad5a9c29cb1afbca1b6978d680af13772
Joenio Costa <joenio@joenio.me>
2017-11-25 17:35:47 -0200

    ensure container doesnt exists before create

--- tests/utf8_support_under_container_test.sh
+++ tests/utf8_support_under_container_test.sh
@@ -7,14 +7,19 @@ oneTimeSetUp() {
 }
 
 test_using_special_characters_under_container() {
-  ./holodev create >> $LOGFILE 2>&1
+  WORKING_DIR=`pwd`
+  TEMP_DIR=`mktemp --directory`
+  cd $TEMP_DIR
+  $WORKING_DIR/holodev create >> $LOGFILE 2>&1
   assertEquals "can't create container" 0 $?
-  ./holodev run echo 'Métodos Ágeis' > utf8.txt 2>&1
+  $WORKING_DIR/holodev run echo 'Métodos Ágeis' > utf8.txt 2>&1
   COUNT=$(cat utf8.txt | grep -x -c 'Métodos Ágeis')
   assertEquals "file content doesn't match 'Métodos Ágeis'" 1 $COUNT
   rm -f utf8.txt
-  ./holodev destroy >> $LOGFILE 2>&1
+  $WORKING_DIR/holodev destroy >> $LOGFILE 2>&1
   assertEquals "can't destroy container" 0 $?
+  cd $WORKING_DIR
+  rm -rf $TEMP_DIR
 }
 
 . shunit2
