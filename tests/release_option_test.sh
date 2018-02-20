119819924febd29c525b7a27baf890166a479938
Joenio Costa <joenio@joenio.me>
2018-02-20 17:19:41 -0300

    fix test for option --release: creating jessie instead of stretch

--- tests/release_option_test.sh
+++ tests/release_option_test.sh
@@ -6,25 +6,25 @@ oneTimeSetUp() {
   ./holodev setup
 }
 
-test_create_DebianStretch_container_using_long_option() {
+test_create_DebianJessie_container_using_long_option() {
   WORKING_DIR=`pwd`
   TEMP_DIR=`mktemp --directory`
   cd $TEMP_DIR
-  $WORKING_DIR/holodev create --release stretch >> $LOGFILE 2>&1
-  COUNT=`$WORKING_DIR/holodev run cat /etc/debian_version | grep --count stretch`
-  assertEquals "container file '/etc/debian_version' doesn't match 'stretch'" 1 $COUNT
+  $WORKING_DIR/holodev create --release jessie >> $LOGFILE 2>&1
+  COUNT=`$WORKING_DIR/holodev run cat /etc/debian_version | grep --count "8.\|jessie"`
+  assertEquals "container file '/etc/debian_version' doesn't match '8.|jessie'" 1 $COUNT
   $WORKING_DIR/holodev destroy >> $LOGFILE 2>&1
   cd $WORKING_DIR
   rm -rf $TEMP_DIR
 }
 
-test_create_DebianStretch_container_using_short_option() {
+test_create_DebianJessie_container_using_short_option() {
   WORKING_DIR=`pwd`
   TEMP_DIR=`mktemp --directory`
   cd $TEMP_DIR
-  $WORKING_DIR/holodev create -r stretch >> $LOGFILE 2>&1
-  COUNT=`$WORKING_DIR/holodev run cat /etc/debian_version | grep --count stretch`
-  assertEquals "container file '/etc/debian_version' doesn't match 'stretch'" 1 $COUNT
+  $WORKING_DIR/holodev create -r jessie >> $LOGFILE 2>&1
+  COUNT=`$WORKING_DIR/holodev run cat /etc/debian_version | grep --count "8.\|jessie"`
+  assertEquals "container file '/etc/debian_version' doesn't match '8.|jessie'" 1 $COUNT
   $WORKING_DIR/holodev destroy >> $LOGFILE 2>&1
   cd $WORKING_DIR
   rm -rf $TEMP_DIR
