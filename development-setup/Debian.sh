d033c35d38a7a70ea0ef709842b3e43a38aa8a3c
Joenio Costa <joenio@joenio.me>
2018-07-24 13:23:49 -0300

    apt-get --force-yes deprecated

--- development-setup/Debian.sh
+++ development-setup/Debian.sh
@@ -2,5 +2,5 @@ export DEBIAN_FRONTEND=noninteractive
 echo "lxc lxc/directory string /var/lib/lxc" | debconf-set-selections
 apt-get update
 which mk-build-deps || apt-get install -y devscripts equivs
-dpkg-checkbuilddeps || apt-get remove --yes --force-yes $(dpkg-checkbuilddeps 2>&1 | grep 'Build conflicts:' | sed 's/.\+Build conflicts: //')
+dpkg-checkbuilddeps || apt-get remove --yes $(dpkg-checkbuilddeps 2>&1 | grep 'Build conflicts:' | sed 's/.\+Build conflicts: //')
 dpkg-checkbuilddeps || mk-build-deps --install --remove --tool 'apt-get --yes --force-yes'
