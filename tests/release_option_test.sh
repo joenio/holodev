0c4c8805ccb06945731fdd1510ae093afb617f3d
Joenio Costa <joenio@joenio.me>
2019-06-26 18:14:03 -0300

    fix tests and docs to point to defalt Debian Stretch

--- tests/release_option_test.sh
+++ tests/release_option_test.sh
@@ -10,9 +10,9 @@ test_create_DebianJessie_container_using_long_option() {
   WORKING_DIR=`pwd`
   TEMP_DIR=`mktemp --directory`
   cd $TEMP_DIR
-  $WORKING_DIR/holodev create --release jessie >> $LOGFILE 2>&1
-  COUNT=`$WORKING_DIR/holodev run cat /etc/debian_version | grep --count "8.\|jessie"`
-  assertEquals "container file '/etc/debian_version' doesn't match '8.|jessie'" 1 $COUNT
+  $WORKING_DIR/holodev create --release stretch >> $LOGFILE 2>&1
+  COUNT=`$WORKING_DIR/holodev run cat /etc/debian_version | grep --count "9.\|stretch"`
+  assertEquals "container file '/etc/debian_version' doesn't match '9.|stretch'" 1 $COUNT
   $WORKING_DIR/holodev destroy >> $LOGFILE 2>&1
   cd $WORKING_DIR
   rm -rf $TEMP_DIR
@@ -22,9 +22,9 @@ test_create_DebianJessie_container_using_short_option() {
   WORKING_DIR=`pwd`
   TEMP_DIR=`mktemp --directory`
   cd $TEMP_DIR
-  $WORKING_DIR/holodev create -r jessie >> $LOGFILE 2>&1
-  COUNT=`$WORKING_DIR/holodev run cat /etc/debian_version | grep --count "8.\|jessie"`
-  assertEquals "container file '/etc/debian_version' doesn't match '8.|jessie'" 1 $COUNT
+  $WORKING_DIR/holodev create -r stretch >> $LOGFILE 2>&1
+  COUNT=`$WORKING_DIR/holodev run cat /etc/debian_version | grep --count "9.\|stretch"`
+  assertEquals "container file '/etc/debian_version' doesn't match '9.|stretch'" 1 $COUNT
   $WORKING_DIR/holodev destroy >> $LOGFILE 2>&1
   cd $WORKING_DIR
   rm -rf $TEMP_DIR
